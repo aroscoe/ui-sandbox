3f383aa773ec4d0ddfe5d8bec5c59f1787f0578c
Anthony Roscoe <aroscoe@limewire.com>
2010-01-06 15:43:36 -0500

    css-resize bugs fixed

--- assets/js/css-resize.js
+++ assets/js/css-resize.js
@@ -3,12 +3,18 @@ var MainFrame = {
     footer: null,
     body:   null,
     nav:    null,
+    navExtraHeight: null,
+    main:   null,
+    mainExtraHeight: null,
 
     initialize: function() {
         MainFrame.header = $("header");
         MainFrame.footer = $("footer");
         MainFrame.body   = $("section#body");
         MainFrame.nav    = $("nav");
+        MainFrame.navExtraHeight = MainFrame.nav.height() - MainFrame.nav.innerHeight();
+        MainFrame.main   = $("section#main");
+        MainFrame.mainExtraHeight = MainFrame.main.height() - MainFrame.main.innerHeight();
 
         MainFrame.setBodyHeight();
 
@@ -19,7 +25,8 @@ var MainFrame = {
     setBodyHeight: function() {
         var height = $(window).height() - MainFrame.header.height() - MainFrame.footer.height();
         MainFrame.body.height(height);
-        MainFrame.nav.height(height);
+        MainFrame.nav.height(height + MainFrame.navExtraHeight);
+        MainFrame.main.height(height + MainFrame.mainExtraHeight);
   }
 }
 

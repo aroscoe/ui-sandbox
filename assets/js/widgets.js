5911eb202008412487ead71c89d846fa1404d6a6
Justin Camerer <iamjwc@gmail.com>
2010-01-06 14:47:50 -0500

    Modified widget syntax a bit

--- assets/js/widgets.js
+++ assets/js/widgets.js
@@ -1,48 +1,38 @@
 
-/*
-$('body').appendWidget("dialog-box", {title: "Hi", description: "This is a dialog box"}, function(self) {
-  $(self).find('.field-buttons button').click(function() {
-    alert("Ok clicked!");
-  });
-  $(self).find('.field-buttons a').click(function() {
-    alert("Cancel clicked!");
-  });
-});
-*/
-
 $.fn.extend({
-  loadWidget: function(widgetName, values, onLoad) {
+  widgetCache: {},
+
+  widgetLoad: function(widgetName, fields, onLoad) {
     var self = this;
 
     self.load(widgetName + ".html #widget-container > *", function() {
-      self.replaceWidgetValues(values);
+      self.widgetPopulate(fields);
       onLoad(self);
     });
 
     return self;
   },
 
-  appendWidget: function(widgetName, values, onLoad) {
+  widgetAppend: function(widgetName, fields, onLoad) {
     var self = this;
 
     var containerDiv = $('<div></div>');
     containerDiv.load(widgetName + ".html #widget-container > *", function() {
-      containerDiv.replaceWidgetValues(values);
+      containerDiv.widgetPopulate(fields);
       onLoad(containerDiv);
     });
 
     return self;
   },
 
-  replaceWidgetValues: function(values) {
-    for(var name in values) {
-      var value = values[name];
-
-      this.findWidgetField(name).html(value);
+  widgetPopulate: function(fields) {
+    for(var name in fields) {
+      this.widgetField(name).html(fields[name]);
     }
   },
 
-  findWidgetField: function(name) {
+  widgetField: function(name) {
     return this.find('.field-' + name);
   }
 });
+
